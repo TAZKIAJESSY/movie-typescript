a1dcd133ed5c304a27cc3d1a505b2b0d03e2d2d8
tazkiajessy <jessy566eee@gmail.com>
2021-03-29 22:24:34 +0200

    just react homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome!</h1>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
